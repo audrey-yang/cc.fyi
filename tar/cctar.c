ce21edaa651a821b93959dfac65bc62e19eef13c
Audrey Yang <ayang24.robin@gmail.com>
2024-04-08 20:24:36 -0500

    TAR: arg -t and -tf

--- tar/cctar.c
+++ tar/cctar.c
@@ -2,21 +2,28 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int list_tar();
 
 int main(int argc, char** argv) {
 	printf("CC TAR\n");
-	// process -t
-	list_tar();
+	if (strcmp(argv[1], "-t") == 0) {
+		list_tar(STDIN_FILENO);
+	} else if (strcmp(argv[1], "-tf") == 0) {
+		int fileno = open(argv[2], 0);
+		list_tar(fileno);
+	}
 	return 0;
 }
 
-int list_tar() {
+int list_tar(int fileno) {
 	char buf[512];
 	int amount_to_read = -1, already_read = 0;
-	while (read(STDIN_FILENO, buf, 512)) {
+	while (read(fileno, buf, 512)) {
 		if (buf[0] == 0) continue;
 		if (amount_to_read < 0) {
 			char name[100];
