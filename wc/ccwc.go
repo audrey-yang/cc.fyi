36a05bcb109a87fda303752d0878bc38c8468fdb
Audrey Yang <ayang.penn@gmail.com>
2025-04-08 19:24:45 -0500

    WC: process -l flag

--- wc/ccwc.go
+++ wc/ccwc.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"io"
     "os"
+	"bufio"
 )
 
 func countBytes(f *os.File) (int64) {
@@ -15,8 +16,24 @@ func countBytes(f *os.File) (int64) {
 	return offset
 }
 
+func countLines(f *os.File) (int64) {
+	scanner := bufio.NewScanner(f)
+
+	numLines := int64(0)
+	for scanner.Scan() {
+        numLines++
+    }
+
+    if err := scanner.Err(); err != nil {
+        panic(err)
+    }
+
+	return numLines
+}
+
 func main() {
 	cPtr := flag.Bool("c", false, "count number of bytes")
+	lPtr := flag.Bool("l", false, "count number of lines")
 	flag.Parse()
 
 	fileName := flag.Arg(0)
@@ -28,6 +45,8 @@ func main() {
 	
 	if *cPtr {
 		fmt.Println("  ", countBytes(f), fileName)
+	} else if *lPtr {
+		fmt.Println("  ", countLines(f), fileName)
 	}
 	
 	defer f.Close()
