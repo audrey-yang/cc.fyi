4d784a62e73c591b68e365263f8ed5fd0ecc735b
Audrey Yang <ayang.penn@gmail.com>
2025-04-11 18:25:37 -0500

    JSON Parser: refactor and add more descriptive error messages

--- json-parser/parser.go
+++ json-parser/parser.go
@@ -7,6 +7,47 @@ import (
 	"strconv"
 )
 
+func tryParse(dataPtr *map[string]interface{}, keyPtr *string, tPtr *string) bool {
+	data := *dataPtr
+	key := *keyPtr
+	t := *tPtr
+
+	// Try to parse as int
+	intVal, err := strconv.Atoi(t)
+	if err == nil {
+		data[key] = intVal
+		return true
+	}
+
+	// Try to parse as bool
+	if t == "true" {
+		data[key] = true
+		return true
+	}
+
+	if t == "false" {
+		data[key] = false
+		return true
+	}
+
+	// Try to parse null
+	if t == "null" {
+		data[key] = nil
+		return true
+	}
+
+	// Try to parse as string
+	strVal, err := strconv.Unquote(t)
+	if err == nil {
+		data[key] = strVal
+		return true
+	}
+
+	fmt.Println("Error parsing value:", t, "for key", key)
+	os.Exit(1)
+	return false
+}
+
 func main() {
 	fileName := os.Args[1]
 	f, err := os.Open(fileName)
@@ -22,6 +63,7 @@ func main() {
 	key := ""
 	valid := false
 	keepScanning := false
+
 	for scanner.Scan() {
 		t := scanner.Text()
 
@@ -36,27 +78,27 @@ func main() {
 			keepScanning = true
 		} else if t == "}" {
 			if keepScanning {
-				valid = false
-				break
+				fmt.Println("Error: Unmatched closing brace")
+				os.Exit(1)
 			}
 		} else if keepScanning && t[len(t)-1] == ':' {
 			maybeKey, err := strconv.Unquote(t[:len(t)-1])
 			if err != nil {
-				valid = false
-				break
+				fmt.Println("Error parsing key:", t)
+				os.Exit(1)
 			}
 			key = maybeKey
 		} else if t[0] == '{' && t[len(t)-1] == ':' {
 			maybeKey, err := strconv.Unquote(t[1 : len(t)-1])
 			if err != nil {
-				valid = false
-				break
+				fmt.Println("Error parsing key:", t)
+				os.Exit(1)
 			}
 			key = maybeKey
 		} else {
 			if key == "" {
-				valid = false
-				break
+				fmt.Println("Error: Key not found for value:", t)
+				os.Exit(1)
 			}
 			if t[len(t)-1] == ',' {
 				keepScanning = true
@@ -68,47 +110,20 @@ func main() {
 				keepScanning = false
 			}
 
-			intVal, err := strconv.Atoi(t)
-			if err == nil {
-				data[key] = intVal
-				continue
-			}
-
-			if t == "true" {
-				data[key] = true
-				continue
-			}
-
-			if t == "false" {
-				data[key] = false
-				continue
-			}
-
-			if t == "null" {
-				data[key] = nil
-				continue
-			}
-
-			strVal, err := strconv.Unquote(t)
-			if err == nil {
-				data[key] = strVal
-				continue
-			}
-
-			valid = false
-			break
+			valid = tryParse(&data, &key, &t)
 		}
 	}
 
 	if keepScanning {
 		valid = false
+		os.Exit(1)
 	}
 
 	if valid {
-		fmt.Println("Valid JSON")
-		fmt.Println(data)
+		fmt.Println("Parsed JSON:", data)
 		os.Exit(0)
 	}
-	fmt.Println("Invalid JSON")
+
+	fmt.Println("Error: empty file")
 	os.Exit(1)
 }
--- json-parser/test.sh
+++ json-parser/test.sh
@@ -1,25 +1,34 @@
 go build
 
 echo STEP 1
-echo valid
+echo --valid
 ./cc-json-parser tests/step1/valid.json
-echo invalid
+echo --invalid
 ./cc-json-parser tests/step1/invalid.json
 echo
 
 echo STEP 2
-echo valid
+echo --valid
 ./cc-json-parser tests/step2/valid.json
-echo valid
+echo --valid
 ./cc-json-parser tests/step2/valid2.json
-echo invalid
+echo --invalid
 ./cc-json-parser tests/step2/invalid.json
-echo invalid
+echo --invalid
 ./cc-json-parser tests/step2/invalid2.json
 echo
 
 echo STEP 3
-echo valid
+echo --valid
 ./cc-json-parser tests/step3/valid.json
-echo invalid
+echo --invalid
 ./cc-json-parser tests/step3/invalid.json
+echo
+
+echo STEP 4
+echo --valid
+./cc-json-parser tests/step4/valid.json
+echo --valid
+./cc-json-parser tests/step4/valid2.json
+echo --invalid
+./cc-json-parser tests/step4/invalid.json
