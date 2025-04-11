10293e6bda512a91fcb269551f4fad50110f7c85
Audrey Yang <ayang.penn@gmail.com>
2025-04-10 21:36:27 -0500

    JSON Parser: handle simple objects with string keys and string values

--- json-parser/parser.go
+++ json-parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -17,19 +18,73 @@ func main() {
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanWords)
 
-	// var data map[string]interface{}
+	var data map[string]interface{} = make(map[string]interface{})
+	key := ""
 	valid := false
+	keepScanning := false
 	for scanner.Scan() {
 		t := scanner.Text()
+		fmt.Println("/", t, "/")
+
 		if t == "{}" {
 			valid = true
 			continue
 		}
-		fmt.Println(t)
+
+		valid = true
+
+		if t == "{" {
+			keepScanning = true
+		} else if t == "}" {
+			if keepScanning {
+				valid = false
+				break
+			}
+		} else if keepScanning && t[len(t)-1] == ':' {
+			maybeKey, err := strconv.Unquote(t[:len(t)-1])
+			if err != nil {
+				valid = false
+				break
+			}
+			key = maybeKey
+		} else if t[0] == '{' && t[len(t)-1] == ':' {
+			maybeKey, err := strconv.Unquote(t[1 : len(t)-1])
+			if err != nil {
+				valid = false
+				break
+			}
+			key = maybeKey
+		} else {
+			if key == "" {
+				valid = false
+				break
+			}
+			if t[len(t)-1] == ',' {
+				keepScanning = true
+				t = t[:len(t)-1]
+			} else if t[len(t)-1] == '}' {
+				keepScanning = false
+				t = t[:len(t)-1]
+			} else {
+				keepScanning = false
+			}
+			val, err := strconv.Unquote(t)
+			if err != nil {
+				valid = false
+				break
+			}
+			data[key] = val
+			fmt.Println(key, val, ".....")
+		}
+	}
+
+	if keepScanning {
+		valid = false
 	}
 
 	if valid {
 		fmt.Println("Valid JSON")
+		fmt.Println(data)
 		os.Exit(0)
 	}
 	fmt.Println("Invalid JSON")
