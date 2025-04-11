2df99efbe9c6dbdcb6d612700cffaf34641d240b
Audrey Yang <ayang.penn@gmail.com>
2025-04-10 22:06:44 -0500

    JSON Parser: add test script

---
+++ json-parser/test.sh
@@ -0,0 +1,25 @@
+go build
+
+echo STEP 1
+echo valid
+./cc-json-parser tests/step1/valid.json
+echo invalid
+./cc-json-parser tests/step1/invalid.json
+echo
+
+echo STEP 2
+echo valid
+./cc-json-parser tests/step2/valid.json
+echo valid
+./cc-json-parser tests/step2/valid2.json
+echo invalid
+./cc-json-parser tests/step2/invalid.json
+echo invalid
+./cc-json-parser tests/step2/invalid2.json
+echo
+
+echo STEP 3
+echo valid
+./cc-json-parser tests/step3/valid.json
+echo invalid
+./cc-json-parser tests/step3/invalid.json
