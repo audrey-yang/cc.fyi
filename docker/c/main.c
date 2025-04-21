df42c5cd1d6a05057484805feb57b1c7cc17464a
Audrey Yang <ayang.penn@gmail.com>
2025-04-20 19:04:24 -0500

    Docker: checkpoint at namespace creation

--- docker/c/main.c
+++ docker/c/main.c
@@ -1,14 +1,44 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sched.h>
+#include <sys/wait.h>
+
+int child_func(char *argv[]) {
+    if (execvp(argv[2], argv + 2)) {
+        perror("execvp");
+        return 1;
+    }
+    return 0;
+}
+
+int parent_clone(char *argv[]) {
+    sethostname("container", 9);
+    child_func(argv);
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
-    int pid = fork();
-    if (pid > 0) {
-        execvp(argv[2], argv + 2);
-        perror(argv[2]); 
-    } else {
-        printf("Hello\n");
+    const int STACK_SIZE = 65536;
+    char* stack = malloc(STACK_SIZE);
+    if (!stack) {
+        perror("malloc");
+        exit(1);
     }
+
+    unsigned long flags = CLONE_NEWUTS;
+
+    if (clone((void*) parent_clone, stack + STACK_SIZE, flags | SIGCHLD, argv) == -1) {
+        perror("clone");
+        exit(1);
+    }
+    
+    int status;
+    if (wait(&status) == -1) {
+        perror("wait");
+        exit(1);
+    }
+
     return 0;
 }
