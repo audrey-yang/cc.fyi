73d66ff82eaad0659ea2e0fe43a33005cdb6a331
Audrey Yang <ayang.penn@gmail.com>
2025-04-22 21:56:49 -0500

    Docker: run C format

--- docker/c/main.c
+++ docker/c/main.c
@@ -6,32 +6,39 @@
 #include <sys/wait.h>
 #include <sys/mount.h>
 
-
-int parent_clone(char *argv[]) {
+int parent_clone(char *argv[])
+{
     sethostname("container", 9);
 
-    if (mount("/proc", "/home/vagrant/c/alpine-minirootfs-3.21.3-x86/proc", "proc", 0, NULL) == -1) {
+    if (mount("/proc", "/home/vagrant/c/alpine-minirootfs-3.21.3-x86/proc", "proc", 0, NULL) == -1)
+    {
         perror("mount");
         return 1;
     }
 
     chdir("/home/vagrant/c/alpine-minirootfs-3.21.3-x86");
-    if (chroot("/home/vagrant/c/alpine-minirootfs-3.21.3-x86") == -1) {
+    if (chroot("/home/vagrant/c/alpine-minirootfs-3.21.3-x86") == -1)
+    {
         perror("chroot");
         umount("/proc");
         return 1;
     }
-    
+
     int pid = fork();
-    if (pid == 0) {
-        if (execvp(argv[2], argv + 2)) {
+    if (pid == 0)
+    {
+        if (execvp(argv[2], argv + 2))
+        {
             perror("execvp");
             umount("/proc");
             return 1;
         }
-    } else {
+    }
+    else
+    {
         int status;
-        if (wait(&status) == -1) {
+        if (wait(&status) == -1)
+        {
             perror("wait");
             umount("/proc");
             exit(1);
@@ -41,24 +48,28 @@ int parent_clone(char *argv[]) {
     return 0;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     const int STACK_SIZE = 65536;
-    char* stack = malloc(STACK_SIZE);
-    if (!stack) {
+    char *stack = malloc(STACK_SIZE);
+    if (!stack)
+    {
         perror("malloc");
         exit(1);
     }
 
     unsigned long flags = CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS | CLONE_NEWIPC;
 
-    if (clone((void*) parent_clone, stack + STACK_SIZE, flags | SIGCHLD, argv) == -1) {
+    if (clone((void *)parent_clone, stack + STACK_SIZE, flags | SIGCHLD, argv) == -1)
+    {
         perror("clone");
         free(stack);
         exit(1);
     }
-    
+
     int status;
-    if (wait(&status) == -1) {
+    if (wait(&status) == -1)
+    {
         perror("wait");
         free(stack);
         exit(1);
