c069af96853dac4887eb0649f6efd68d585cba36
Audrey Yang <ayang.penn@gmail.com>
2025-05-05 22:01:09 -0500

    Traceroute: send packets with increasing TTL and record times

--- traceroute/cctraceroute/main.cc
+++ traceroute/cctraceroute/main.cc
@@ -2,6 +2,7 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <chrono>
 
 int get_ip_addr(struct in_addr &address, char *hostname)
 {
@@ -47,45 +48,64 @@ void print_hostname(struct sockaddr_in &address)
     std::cerr << "(" << hostname << ")";
 }
 
-int send_recv_message(struct in_addr *server_ip, char *message)
+int send_recv_message(struct in_addr *server_ip, char *packet, int max_iters)
 {
     // Open UDP socket for sending
     int sender_sock = socket(AF_INET, SOCK_DGRAM, 0);
 
-    // Set TTL
-    int ttl = 1;
-    setsockopt(sender_sock, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
-
     struct sockaddr_in send_addr;
     send_addr.sin_addr = *server_ip;
     send_addr.sin_family = AF_INET;
-    send_addr.sin_port = htons(33530);
-
-    // Send message
-    int bytes_sent = sendto(sender_sock, message, 32, 0, (sockaddr *)&send_addr, sizeof(send_addr));
-    if (bytes_sent < 0)
-    {
-        std::cerr << "Error: Could not send request" << std::endl;
-        return -1;
-    }
-    close(sender_sock);
+    send_addr.sin_port = htons(33434);
 
     // Open ICMP socket for receiving
     int receiver_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
 
-    // Receive response
-    char response[128];
-    struct sockaddr_in recv_addr;
-    uint32_t recv_addr_len;
-    int bytes_recv = recvfrom(receiver_sock, response, 65535, 0, (struct sockaddr *)&recv_addr, &recv_addr_len);
-    if (bytes_recv < 0)
+    for (int iter = 0; iter < max_iters; iter++)
     {
-        return -1;
+        // Set TTL
+        int ttl = iter;
+        setsockopt(sender_sock, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
+
+        struct timeval tv;
+        tv.tv_sec = 0;
+        tv.tv_usec = 50000;
+        setsockopt(receiver_sock, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof tv);
+
+        // Send message
+        auto start = std::chrono::high_resolution_clock::now();
+        int bytes_sent = sendto(sender_sock, packet, 32, 0, (sockaddr *)&send_addr, sizeof(send_addr));
+        if (bytes_sent < 0)
+        {
+            perror("sendto");
+            return -1;
+        }
+
+        // Receive response
+        char response[128];
+        struct sockaddr_in recv_addr;
+        uint32_t recv_addr_len;
+        int bytes_recv = recvfrom(receiver_sock, response, 65535, 0, (struct sockaddr *)&recv_addr, &recv_addr_len);
+        auto end = std::chrono::high_resolution_clock::now();
+        if (bytes_recv < 0)
+        {
+            std::cerr << iter << "  * * *" << std::endl;
+            continue;
+        }
+
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+        std::cerr << iter << "  " << inet_ntoa(recv_addr.sin_addr) << " ";
+        print_hostname(recv_addr);
+        std::cerr << " " << duration.count() << "ms" << std::endl;
+
+        if (recv_addr.sin_addr.s_addr == server_ip->s_addr)
+        {
+            break;
+        }
     }
 
-    std::cerr << 1 << "\t" << inet_ntoa(recv_addr.sin_addr) << " ";
-    print_hostname(recv_addr);
-    std::cerr << std::endl;
+    close(sender_sock);
     close(receiver_sock);
     return 0;
 }
@@ -100,8 +120,8 @@ int main(int argc, char **argv)
 
     fprintf(stderr, "traceroute to %s (%s), %d hops max, %d byte packets\n", argv[1], inet_ntoa(address), 64, 32);
 
-    char message[32];
-    strcpy(message, "codingchallenges.fyi traceroute");
-    send_recv_message(&address, message);
+    char packet[32];
+    strcpy(packet, "codingchallenges.fyi traceroute");
+    send_recv_message(&address, packet, 64);
     return 0;
 }
