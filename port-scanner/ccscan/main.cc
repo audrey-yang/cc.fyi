46ffb8f2c30107ff8eef57e4f0eb3ca3f396d6b9
Audrey Yang <ayang.penn@gmail.com>
2025-05-11 22:33:04 -0500

    Port Scanner: fix open port check and implement vanilla scan

--- port-scanner/ccscan/main.cc
+++ port-scanner/ccscan/main.cc
@@ -1,7 +1,52 @@
 #include <iostream>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
 #include <cxxopts.hpp>
+
+int get_ip_addr(struct in_addr &address, std::string &hostname)
+{
+    struct addrinfo hints, *result, *p;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = IPPROTO_UDP;
+
+    int status = getaddrinfo(hostname.c_str(), NULL, &hints, &result);
+    if (status)
+    {
+        perror("getaddrinfo");
+        return -1;
+    }
+
+    for (p = result; p != NULL; p = p->ai_next)
+    {
+        struct sockaddr_in *ai_addr = (struct sockaddr_in *)p->ai_addr;
+        address.s_addr = ai_addr->sin_addr.s_addr;
+        break;
+    }
+
+    freeaddrinfo(result);
+
+    return 0;
+}
+
+void check_port(std::string &hostname, uint16_t port)
+{
+    struct sockaddr_in server;
+    server.sin_family = AF_INET;
+    get_ip_addr(server.sin_addr, hostname);
+    server.sin_port = htons(port);
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (connect(sockfd, (struct sockaddr *)&server, sizeof(server)) == 0)
+    {
+        std::cout << "Port: " << port << " is open" << std::endl;
+    }
+    close(sockfd);
+}
 
 int main(int argc, char **argv)
 {
@@ -18,32 +63,24 @@ int main(int argc, char **argv)
     }
     if (result.count("port") > 0)
     {
-        std::cout << "port: " << result["port"].as<int>() << std::endl;
+        std::cout << "port: " << result["port"].as<int>();
     }
+    std::cout << std::endl;
 
     std::string hostname = result["host"].as<std::string>();
-    if (hostname == "localhost")
-    {
-        hostname = "127.0.0.1";
-    }
     int port = result["port"].as<int>();
 
-    struct sockaddr_in server;
-    if (inet_pton(AF_INET, hostname.c_str(), &(server.sin_addr)) == 1)
+    if (port < 0)
     {
-        std::cout << server.sin_addr.s_addr << std::endl;
-    }
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (connect(sockfd, (struct sockaddr *)&server, sizeof(server)))
-    {
-        std::cout << "Port: " << port << " is open" << std::endl;
+        // Perform vanilla scan
+        for (uint16_t port = 1; port != 0; port++)
+        {
+            check_port(hostname, port);
+        }
     }
     else
     {
-        std::cout << "Port: " << port << " is not open" << std::endl;
+        check_port(hostname, port);
     }
 
     return 0;
