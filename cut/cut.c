8e6cb6c96f0133551dcc01519bbf5979bb645e33
Audrey Yang <ayang.penn@gmail.com>
2025-09-03 20:17:27 -0500

    Cut: support delimeter choice

--- cut/cut.c
+++ cut/cut.c
@@ -6,12 +6,15 @@
 
 int main(int argc, char** argv) {
     int section = 0;
-    if (argc == 3) {
+    char delim = '\t';
+    if (argc > 2) {
         if (argv[1][1] == 'f') {
             section = atoi(argv[1] + 2);
         }
+        if (argv[2][1] == 'd') {
+            delim = argv[2][2];
+        }
     }
-    const char *delim = "\t";
 
     FILE *fp;
     fp = fopen(argv[argc-1], "r");
@@ -21,11 +24,11 @@ int main(int argc, char** argv) {
 
     char buf[BUFFER_LEN];
     while (fgets(buf, BUFFER_LEN - 1, fp)) {
-        char* split = strtok(buf, delim);
+        char* split = strtok(buf, &delim);
         char slice[BUFFER_LEN];
         int count = 1;
         while (count < section && split != NULL) {
-            split = strtok (NULL, delim);
+            split = strtok (NULL, &delim);
             count += 1;
         }
         printf("%s\n", split);
