4d84fd1c986f1516f53fbbf75cd17c5f17d28917
Audrey Yang <ayang.penn@gmail.com>
2025-11-09 16:09:32 -0600

    Huffman: implement file read and frequency counting

---
+++ huffman/src/main.rs
@@ -0,0 +1,50 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::collections::HashMap;
+
+fn get_freqs(s: &String) -> HashMap<char, i32> {
+    let mut freqs = HashMap::new();
+    for c in s.chars() { 
+        match freqs.get(&c) {
+            Some(freq) => freqs.insert(c, freq + 1),
+            _ => freqs.insert(c, 1),
+        };
+    }
+    return freqs;
+}
+
+fn print_map_value(mp: &HashMap<char, i32>, k: char) {
+    match mp.get(&k) {
+        Some(v) => println!("Value {} for key {}", v, k),
+        _ => println!("Not found")
+    };
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let filepath = &args[1];
+    println!("Reading the file at {}.", filepath);
+
+    let path = Path::new(filepath);
+    let display = path.display();
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut text = String::new();
+    match file.read_to_string(&mut text) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(size) => println!("Successfully read {}, {} bytes", display, size),
+    }
+
+    let freqs = get_freqs(&text);
+    print_map_value(&freqs, 'X');
+    print_map_value(&freqs, 't');
+}
